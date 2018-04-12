df19e5f45dbb64aab8daf97918f73df2985994ea
蔡雨甫 <yufu.cai@dianrong.com>
2018-04-12 18:27:27 +0800

    lodash string

--- src/helper/dom.utils.ts
+++ src/helper/dom.utils.ts
@@ -1,4 +1,4 @@
-import { isString } from 'lodash'
+import isString from 'lodash/isString'
 
 import { hashCode, navigateAppByName } from './app.helper'
 import { MooaApp } from '../model/IAppOption'
